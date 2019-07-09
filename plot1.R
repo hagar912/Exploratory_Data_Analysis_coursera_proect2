3fa68c9cad798b84f9890cf0dc4a6ad5d1b71144
Hagar Nabil <47554625+hagar912@users.noreply.github.com>
2019-07-10 00:03:07 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,9 @@
+#dowload dataset
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
 # Load the NEI & SCC data frames;
 
 NEI <- readRDS("summarySCC_PM25.rds")
